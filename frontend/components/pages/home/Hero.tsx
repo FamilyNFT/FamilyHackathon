708f32465ed11b0e32390a4cf58e3dbac6639e0f
FamilyNFT <94820455+FamilyNFT@users.noreply.github.com>
2022-09-01 12:22:20 +1000

    Update Hero.tsx

--- frontend/components/pages/home/Hero.tsx
+++ frontend/components/pages/home/Hero.tsx
@@ -8,26 +8,6 @@ function Hero() {
         <h1 className="text-5xl font-bold mb-8 text-red-400">
           The Home of Phygitals
         </h1>
-        <div className="flex flex-col space-y-2 text-gray-200">
-          <span>
-            Made with only the best quality French Terry Cotton Fabric,
-            specifically engineered to balance softness and thickness to
-            maximize comfort
-          </span>
-          <span>
-            With our Classic Script logo embroidered for that luxurious
-            minimalistic look
-          </span>
-          <span>
-            Luxuriously soft and ridiculously cozy, our relaxed unisex fit will
-            keep you looking fresh whilst keeping you warm during the harsh
-            winters. The perfect everyday outfit that gives off a cool luxurious
-            vibe.
-          </span>
-        </div>
-        <div>
-          <div className="hover:bg-blue-600 inline-block bg-blue-500 px-8 py-2.5 rounded mt-5">
-            <Link href={"/order"}>Order Now</Link>
           </div>
         </div>
       </div>
