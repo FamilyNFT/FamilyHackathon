7cf20dbffcbfdca00b4422a7043b36407e9fe5e4
mumeen <azeezmumeen6@gmail.com>
2022-09-03 13:35:13 +0100

    fix errors

--- rest-api/app.js
+++ rest-api/app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const DB_URI = require("./db");
-const itemsRoute = require("./routes/items");
 
-///Connect Database///
+//middlewares//
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+///Routes///
+const itemsRoute = require("./routes/items");
 app.use("/items", itemsRoute);
 
 app.listen(3000, () => console.log("App is listening at port 3000"));
--- rest-api/routes/items.js
+++ rest-api/routes/items.js
@@ -12,5 +12,19 @@ router.get("/", async (request, response) => {
     response.send(error);
   }
 });
+router.post("/", async (req, response) => {
+  try {
+    const items = await Items.create({
+      tokenId: req.body.tokenId,
+      metadata: req.body.metadata,
+      owner: req.body.owner,
+    });
+    console.log(items);
+    response.send(items);
+  } catch (error) {
+    console.log(error);
+    response.send(error);
+  }
+});
 
 module.exports = router;
